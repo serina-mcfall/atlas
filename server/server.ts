f3e72c08f42963afd439c30795f5e8403c39af23
Serina Mcfall <serina.mcfall@gmail.com>
2026-05-25 14:18:55 +1200

    update step 2 Done

--- server/server.ts
+++ server/server.ts
@@ -9,6 +9,16 @@ server.get('/api/v1/artwork', (req, res) => {
   res.json(data)
 })
 
+server.get('/api/v1/artwork/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const artwork = data.find((a) => a.id === id)
+  if (!artwork) {
+    res.sendStatus(404)
+  } else {
+    res.json(artwork)
+  }
+})
+
 export default server
 
 // Routes
